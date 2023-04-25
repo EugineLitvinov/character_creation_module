688c2845a4e023c85a6f921688a863ba8fa47899
Евгения Литвинова <semi-transparent@ya.ru>
2023-04-25 22:08:30 +0300

    Add version without classes

---
+++ temp.py
@@ -0,0 +1,23 @@
+from math import *
+
+
+message = 'Добро пожаловать в самую лучшую программу для вычисления ' \
+          'квадратного корня из заданного числа'
+print(message)
+
+
+def CalculateSquareRoot(Number):
+    """Вычисляет квадратный корень"""
+    return sqrt(Number)
+
+
+def calc(your_number):
+    if your_number <= 0:
+        return
+    root = CalculateSquareRoot(your_number)
+    print("Мы вычислили квадратный корень из введённого вами числа. "
+          f"Это будет: {root}")
+
+
+print(message)
+calc(25.5)
